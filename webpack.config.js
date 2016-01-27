04e44172bd09f30cd1f29427d174e3ff50b77b68
Diwank Singh <diwank.singh@gmail.com>
2016-01-27 18:37:09 -0500

    Fix weback config
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-var webpack = require('webpack');  
+var webpack = require('webpack');
 
 if ( process.env.NODE_ENV !== "production" ) {
-    module.exports = {  
+    module.exports = {
         entry: [
           'webpack/hot/only-dev-server',
           "./js/app.js"
@@ -28,16 +28,30 @@ if ( process.env.NODE_ENV !== "production" ) {
         plugins: [
           new webpack.NoErrorsPlugin()
         ]
-    
+
     };
 } else {
-    module.exports = {  
+    module.exports = {
         entry: [
           "./js/app.js"
         ],
         output: {
             path: __dirname + '/build',
             filename: "bundle.js"
-        }
+        },
+        module: {
+          loaders: [
+            {
+              test: /\.jsx?$/,
+              loaders: ['react-hot', 'babel-loader?presets[]=react,presets[]=es2015'],
+              exclude: /node_modules/
+            },
+            { test: /\.css$/, loader: "style!css" }
+          ],
+        },
+        plugins: [
+          new webpack.NoErrorsPlugin()
+        ]
+
     };
 }
