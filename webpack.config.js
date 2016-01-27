b9742c4d9737ebde8baca229517f776f0880aa3c
Diwank Singh Tomer <singh@diwank.name>
2016-01-27 18:17:52 -0500

    Added a production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,43 @@
 var webpack = require('webpack');  
-module.exports = {  
-    entry: [
-      'webpack/hot/only-dev-server',
-      "./js/app.js"
-    ],
-    output: {
-        path: __dirname + '/build',
-        filename: "bundle.js"
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js?$/,
-          loaders: ['react-hot', 'babel-loader?presets[]=react,presets[]=es2015'],
-          exclude: /node_modules/
-        },
-        {
-          test: /\.jsx?$/,
-          loaders: ['react-hot', 'babel-loader?presets[]=react,presets[]=es2015'],
-          exclude: /node_modules/
-        },
-        { test: /\.css$/, loader: "style!css" }
-      ],
-    },
-    plugins: [
-      new webpack.NoErrorsPlugin()
-    ]
 
-};
+if ( process.env.NODE_ENV !== "production" ) {
+    module.exports = {  
+        entry: [
+          'webpack/hot/only-dev-server',
+          "./js/app.js"
+        ],
+        output: {
+            path: __dirname + '/build',
+            filename: "bundle.js"
+        },
+        module: {
+          loaders: [
+            {
+              test: /\.js?$/,
+              loaders: ['react-hot', 'babel-loader?presets[]=react,presets[]=es2015'],
+              exclude: /node_modules/
+            },
+            {
+              test: /\.jsx?$/,
+              loaders: ['react-hot', 'babel-loader?presets[]=react,presets[]=es2015'],
+              exclude: /node_modules/
+            },
+            { test: /\.css$/, loader: "style!css" }
+          ],
+        },
+        plugins: [
+          new webpack.NoErrorsPlugin()
+        ]
+    
+    };
+} else {
+    module.exports = {  
+        entry: [
+          "./js/app.js"
+        ],
+        output: {
+            path: __dirname + '/build',
+            filename: "bundle.js"
+        }
+    };
+}
